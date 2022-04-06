d5049d02289ba458f659b8c6d7f90830bde1db73
LiJin <jin.l@medicalsystem.cn>
2022-04-06 17:27:45 +0800

    feat: 统一引入接口

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,17 @@
-import demo from './demo'
+const Apis = {}
 
-export default {
-  demo
+const context = require.context('./', true, /\.js$/)
+const req = context => {
+  return context.keys().map(item => ({
+    name: item.replace(/(.*\/)*([^.]+).*/gi, '$2'),
+    apis: context(item)
+  }))
 }
+const options = req(context)
+options.forEach(option => {
+  Object.assign(Apis, {
+    [option.name]: { ...option.apis.default }
+  })
+})
+
+export default Apis
